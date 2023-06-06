d3d971eb9c059f6986eb1cb06a16e2267cf61044
mbe <martin.beckmann@mediaopt.de>
2023-06-06 09:35:28 +0200

    #34190 unittests for transaction sale requests

--- braintree-extension/tests/braintree.service.spec.ts
+++ braintree-extension/tests/braintree.service.spec.ts
@@ -1,5 +1,8 @@
 import { describe, expect, test } from '@jest/globals';
-import { getClientToken } from '../src/service/braintree.service';
+import {
+  getClientToken,
+  transactionSale,
+} from '../src/service/braintree.service';
 
 describe('Testing Braintree GetClient Token', () => {
   test('create client token', async () => {
@@ -27,4 +30,45 @@ describe('Testing Braintree GetClient Token', () => {
       Error
     );
   });
+});
+
+describe('Testing Braintree Transaction Sale', () => {
+  test.each([
+    {
+      name: 'credit card',
+      nonce: 'fake-valid-nonce',
+      expectedPaymentInstrumentType: 'credit_card',
+    },
+    {
+      name: 'paypal',
+      nonce: 'fake-paypal-billing-agreement-nonce',
+      expectedPaymentInstrumentType: 'paypal_account',
+    },
+    {
+      name: 'google pay',
+      nonce: 'fake-android-pay-nonce',
+      expectedPaymentInstrumentType: 'android_pay_card',
+    },
+    {
+      name: 'venmo',
+      nonce: 'fake-venmo-account-nonce',
+      expectedPaymentInstrumentType: 'venmo_account',
+    },
+  ])(
+    '$name transaction',
+    async ({ nonce, expectedPaymentInstrumentType }) => {
+      const response = expect(
+        transactionSale({
+          amount: '100',
+          paymentMethodNonce: nonce,
+        })
+      ).resolves;
+      await response.toHaveProperty('status', 'authorized');
+      await response.toHaveProperty(
+        'paymentInstrumentType',
+        expectedPaymentInstrumentType
+      );
+    },
+    10000
+  );
 });
