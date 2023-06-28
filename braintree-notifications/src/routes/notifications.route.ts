eb2584f0a337a8fe60baded91b52e4a79452e30b
mbe <martin.beckmann@mediaopt.de>
2023-06-28 09:48:13 +0200

    #34077 bugfix for async error handling

--- braintree-notifications/src/routes/notifications.route.ts
+++ braintree-notifications/src/routes/notifications.route.ts
@@ -1,8 +1,18 @@
-import { Router } from 'express';
+import { Router, NextFunction, Request, Response } from 'express';
+
 import { post } from '../controllers/notifications.controller';
 
 const notificationRouter: Router = Router();
 
-notificationRouter.post('/', post);
+notificationRouter.post(
+  '/',
+  async function (req: Request, res: Response, next: NextFunction) {
+    try {
+      return await post(req, res);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 export default notificationRouter;
