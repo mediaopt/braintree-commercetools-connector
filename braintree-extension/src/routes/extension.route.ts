93aa5d59066324488ae3776afee6422a6074a7c1
mbe <martin.beckmann@mediaopt.de>
2023-06-29 09:05:03 +0200

    #34077 code review changes

--- braintree-extension/src/routes/extension.route.ts
+++ braintree-extension/src/routes/extension.route.ts
@@ -1,18 +1,8 @@
-import { Router, NextFunction, Request, Response } from 'express';
-
+import { Router } from 'express';
 import { post } from '../controllers/extension.controller';
 
 const braintreeExtensionRouter: Router = Router();
 
-braintreeExtensionRouter.post(
-  '/',
-  async function (req: Request, res: Response, next: NextFunction) {
-    try {
-      return await post(req, res);
-    } catch (error) {
-      next(error);
-    }
-  }
-);
+braintreeExtensionRouter.post('/', post);
 
 export default braintreeExtensionRouter;
--- braintree-notifications/src/routes/notifications.route.ts
+++ braintree-notifications/src/routes/notifications.route.ts
@@ -1,18 +1,8 @@
-import { Router, NextFunction, Request, Response } from 'express';
-
+import { Router } from 'express';
 import { post } from '../controllers/notifications.controller';
 
 const notificationRouter: Router = Router();
 
-notificationRouter.post(
-  '/',
-  async function (req: Request, res: Response, next: NextFunction) {
-    try {
-      return await post(req, res);
-    } catch (error) {
-      next(error);
-    }
-  }
-);
+notificationRouter.post('/', post);
 
 export default notificationRouter;
