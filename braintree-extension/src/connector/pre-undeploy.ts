d40de8b1c332c3e3088e341937166a8063e58634
liudmyla <liudmyla.masliuk@mediaopt.de>
2025-06-20 13:38:48 +0200

    38346 - added delete custom types if possible on pre-undeploy

--- braintree-extension/src/connector/pre-undeploy.ts
+++ braintree-extension/src/connector/pre-undeploy.ts
@@ -1,19 +1,33 @@
 import * as dotenv from 'dotenv';
-
-dotenv.config();
-
 import { createApiRoot } from '../client/create.client';
 import { assertError } from '../utils/assert.utils';
 import {
   BRAINTREE_CUSTOMER_EXTENSION_KEY,
+  BRAINTREE_CUSTOMER_TYPE_KEY,
   BRAINTREE_EXTENSION_KEY,
+  BRAINTREE_PAYMENT_INTERACTION_TYPE_KEY,
+  BRAINTREE_PAYMENT_TRANSACTION_TYPE_KEY,
+  BRAINTREE_PAYMENT_TYPE_KEY,
   deleteExtensionIfExist,
+  deleteOrUpdateCustomType,
 } from './actions';
+
+dotenv.config();
 
 async function preUndeploy(): Promise<void> {
   const apiRoot = createApiRoot();
   await deleteExtensionIfExist(apiRoot, BRAINTREE_EXTENSION_KEY);
   await deleteExtensionIfExist(apiRoot, BRAINTREE_CUSTOMER_EXTENSION_KEY);
+  await deleteOrUpdateCustomType(apiRoot, BRAINTREE_PAYMENT_TYPE_KEY);
+  await deleteOrUpdateCustomType(
+    apiRoot,
+    BRAINTREE_PAYMENT_TRANSACTION_TYPE_KEY
+  );
+  await deleteOrUpdateCustomType(
+    apiRoot,
+    BRAINTREE_PAYMENT_INTERACTION_TYPE_KEY
+  );
+  await deleteOrUpdateCustomType(apiRoot, BRAINTREE_CUSTOMER_TYPE_KEY);
 }
 
 async function run(): Promise<void> {
