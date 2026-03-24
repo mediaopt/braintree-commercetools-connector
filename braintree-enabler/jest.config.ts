5bb66bd508af4ec352510a9ebec2fb7ded08ca86
liudmyla <liudmyla.masliuk@mediaopt.de>
2026-03-24 16:22:13 +0100

    36826 - switched from obsolete react-scripts to vite

---
+++ braintree-enabler/jest.config.ts
@@ -0,0 +1,8 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+
+export default {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  setupFiles: ["./test/jest.setup.ts"],
+  roots: ["./test"],
+};
---
+++ braintree-enabler/vite.config.ts
@@ -0,0 +1,18 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: resolve(__dirname, "public"),
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, "src/main.ts"),
+      name: "Connector",
+      formats: ["es", "umd"],
+      // the proper extensions will be added
+      fileName: (format) => `connector-enabler.${format}.js`,
+    },
+  },
+});
