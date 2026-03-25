62ad114e6241eb0901d5eea6884eac99bb2dbcef
liudmyla <liudmyla.masliuk@mediaopt.de>
2026-03-25 11:37:53 +0100

    36826 - removed some obsolete package.json commands and enabled local development for enabler

---
+++ braintree-enabler/src/index.ts
@@ -0,0 +1,17 @@
+export { PayPal } from "./components/PayPal";
+export { CreditCard } from "./components/CreditCard";
+export { GooglePay } from "./components/GooglePay";
+export { Venmo } from "./components/Venmo";
+export { ApplePay } from "./components/ApplePay";
+export { ACH } from "./components/ACH";
+export {
+  Bancontact,
+  P24,
+  Sofort,
+  EPS,
+  Giropay,
+  Grabpay,
+  IDeal,
+  MyBank,
+  BLIK,
+} from "./components/LocalPaymentMethods";
