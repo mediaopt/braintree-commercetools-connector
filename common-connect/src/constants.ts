961a961b8621f64680ac6a5a82dfff9199e2df93
liudmyla <liudmyla.masliuk@mediaopt.de>
2026-04-20 12:43:58 +0200

    36826 - added constants for customer type and vault defaults to common connect

--- common-connect/src/constants.ts
+++ common-connect/src/constants.ts
@@ -1,3 +1,13 @@
 export const BRAINTREE_PAYMENT_TYPE_KEY = "braintree-payment-type";
 export const BRAINTREE_PAYMENT_INTERACTION_TYPE_KEY =
   "braintree-payment-interaction-type";
+
+export const BRAINTREE_CUSTOMER_TYPE_KEY = "braintree-customer-type";
+
+export const VAULT_BRAINTREE_OPTIONS = {
+  failOnDuplicatePaymentMethod: true,
+  usBankAccountVerificationMethod: "network_check",
+  verifyCard: process.env.BRAINTREE_VALIDATE_CARD === "true" || undefined,
+  verificationMerchantAccountId:
+    process.env.BRAINTREE_MERCHANT_ACCOUNT || undefined,
+};
