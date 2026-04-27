5af680050432c2f5c191a00254b812089bb0697b
liudmyla <liudmyla.masliuk@mediaopt.de>
2026-04-27 13:34:37 +0200

    36826 - cleaned the code

--- braintree-processor/src/server/plugins/braintree-payment.plugin.ts
+++ braintree-processor/src/server/plugins/braintree-payment.plugin.ts
@@ -4,13 +4,13 @@ import { paymentRoutes } from '../../routes/braintree-payment.route';
 import { BraintreePaymentService } from '../../services/braintree-payment.service';
 
 export default async function (server: FastifyInstance) {
-  const mockPaymentService = new BraintreePaymentService({
+  const braintreePaymentService = new BraintreePaymentService({
     ctCartService: paymentSDK.ctCartService,
     ctPaymentService: paymentSDK.ctPaymentService,
   });
 
   await server.register(paymentRoutes, {
-    paymentService: mockPaymentService,
+    paymentService: braintreePaymentService,
     sessionHeaderAuthHook: paymentSDK.sessionHeaderAuthHookFn,
   });
 }
