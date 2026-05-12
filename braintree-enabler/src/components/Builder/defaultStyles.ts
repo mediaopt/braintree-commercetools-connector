8f14f5768d3b207fb02dccde1beaacbd8205b46f
liudmyla <liudmyla.masliuk@mediaopt.de>
2026-05-12 21:16:39 +0200

    36826 - minor style fixes for PayPal

--- braintree-enabler/src/components/Builder/defaultStyles.ts
+++ braintree-enabler/src/components/Builder/defaultStyles.ts
@@ -16,39 +16,42 @@ export const ApplePayDefaultStyleProps = {
   applePayDisplayName: "My Store",
 };
 
+const PayPalCommonDefaultStyles = {
+  buttonColor: "gold" as ButtonColorOption,
+  shape: "pill" as ButtonShapeOption,
+  size: "small" as ButtonSizeOption,
+  payLaterButtonColor: "gold" as ButtonColorOption,
+  locale: "en_GB",
+};
+
 export const PayPalDefaultStyleProps = {
   // flow: "checkout", //todo - move commented out to config or to base options
   // intent: "capture",
   //useKount: true,
-  buttonColor: "blue" as ButtonColorOption,
+  ...PayPalCommonDefaultStyles,
   buttonLabel: "pay" as ButtonLabelOption,
   payLater: true,
-  payLaterButtonColor: "blue" as ButtonColorOption,
-  locale: "en_GB",
-  shape: "pill" as ButtonShapeOption,
-  size: "small" as ButtonSizeOption,
   tagline: true,
   height: 55,
 };
 
 export const PayPalExpressStyleProps = {
+  ...PayPalCommonDefaultStyles,
   flow: "checkout" as FlowType, //required
-  buttonColor: "gold" as ButtonColorOption,
   buttonLabel: "buynow" as ButtonLabelOption,
   payLater: false,
   commit: true,
-  locale: "en_GB",
   intent: "capture" as Intent,
   enableShippingAddress: true,
   shippingAddressEditable: true,
 };
 
 export const PayPalVaultStyleProps = {
+  ...PayPalCommonDefaultStyles,
   flow: "vault", //required for pure vault
-  buttonColor: "blue" as ButtonColorOption,
+  buttonColor: "gold" as ButtonColorOption,
   buttonLabel: "pay" as ButtonLabelOption,
   payLater: false,
   commit: false,
-  locale: "en_GB",
   intent: "tokenize" as Intent,
 };
