fcda552a241ab61a2a612d090c0ccfd56e4603d0
liudmyla <liudmyla.masliuk@mediaopt.de>
2026-06-02 11:47:49 +0200

    36826 - recommended by commercetools env handling

--- enabler/vite.config.ts
+++ enabler/vite.config.ts
@@ -4,11 +4,14 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react(), tailwindcss(), cssInjectedByJsPlugin()],
   build: {
     outDir: resolve(__dirname, "public"),
     lib: {
+      define: {
+        "process.env.NODE_ENV": JSON.stringify(mode),
+      },//recommended by commercetools based on demo project experience
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, "src/main.ts"),
       name: "Connector",
@@ -17,4 +20,4 @@ export default defineConfig({
       fileName: (format) => `connector-enabler.${format}.js`,
     },
   },
-});
+}));
